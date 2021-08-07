cdc8c4e9b3d330ee1d7fb95f80448d0b1e940d4b
张明轩 <zhangmingxuan@s-MacBook-puro.local>
2021-08-07 15:09:14 +0800

    Completed user functionality

--- application.py
+++ application.py
@@ -3,16 +3,19 @@ from flask.wrappers import Response
 from flast_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 from elasticsearch import Elasticsearch
+import uuid
 
-
+# Connect to Elasticsearch
 es = Elasticsearch(["https://es-8xbmi48v.public.tencentelasticsearch.com:9200/"])
 es.cluster.health(wait_for_status='yellow', request_timeout=1)
 
+# Create the application instance
 app = Flask(__name__)
 
 
 class User():
     def __init__(self, username, password):
+        self._id = str(uuid.uuid4())
         self.username = username
         self.password = password
 
@@ -42,10 +45,20 @@ def new_user():
         abort(Response("Username cannot be empty")) # missing arguments
     user = user_index.search({"query": {"term": {'username': username}}})
     if user['hits']['total'] > 0:
-        abort(Response("Username already exits")
+        abort(Response("Username already exits")) # existing user
     user = User(username, generate_password_hash(password))
     user_index.put_doc(user.__dict__)
-    
+    return jsonify({'username': user.username}), 201, {'Location': url_for('get_user', id = user._id, _external = True)}
+
+
+@app.route('/api/users/<id>')
+def get_user(id):
+    user_index = ESIndex("user")
+    user = user_index.get_doc(id)
+    if not user:
+        abort(Response("User not found")) # user not found
+    return jsonify({'username': user['_source']['username']})
+
 
 @auth.verify_password
 def verify_password(username, password):
